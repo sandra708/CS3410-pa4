5e270378d4d49595590bf4a7f939be73de00f785
sandra708 <aea84@cornell.edu>
2015-05-12 13:58:06 -0400

    return to integer speed measurements

--- stats.c
+++ stats.c
@@ -7,6 +7,8 @@ void update_stats(volatile struct global_stats* stats, struct packet_info *curre
     stats->total_packets++;
     stats->bytes_handled += current_packet->packet_length;
 
+    //printf("On update: total packets = %d, total bytes = %d.\n", stats->total_packets, stats->bytes_handled);
+
     if(code & 1) stats->total_spam++;
     if(code & 2) stats->total_vul++;
     if(code & 4) stats->total_evil++;
@@ -18,11 +20,13 @@ void simple_stats_print(volatile struct global_stats* stats){
 	spin_lock(&stats->lock);
 
     int cycle = current_cpu_cycles();
-    int secs1 = ((double) (cycle - stats->time_start))/((double) CPU_CYCLES_PER_SECOND);
+    printf("Cycles: %d.\n", cycle - stats->time_start);
+    int secs1 = ((int) (cycle - stats->time_start))/((int) CPU_CYCLES_PER_SECOND);
+    printf("Seconds: %d.\n", secs1);
     //int secs2=(cycle-last_print)/CPU_CYCLES_PER_SECOND;
     //last_print=cycle;
-    int r1 = stats->total_packets/secs1;
-    int r2 = stats->bytes_handled/secs1;
+    int r1 = ((int) stats->total_packets)/secs1;
+    int r2 = ((int) stats->bytes_handled)/secs1;
     //int r3=total_packets/(secs2+1);
     //int r4=bytes_handled/(secs2+1);
     printf("----------totals---------\n");
